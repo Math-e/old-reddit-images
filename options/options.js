e3c4d117f20aeb343b62ee26b15454a6f3d0d723
Matheus Lima <matheus.limadasilva@outlook.com>
2024-09-24 13:43:55 -0300

    Added option to open on same or new tab; reworked save function

--- options/options.js
+++ options/options.js
@@ -1,24 +1,32 @@
-function saveOptions(e) {
+function saveOption(e) {
   e.preventDefault();
+  const param = e.target.name
+  const value = e.target.value
   browser.storage.sync.set({
-    maxHeight: document.querySelector("#maxHeight").value,
+    [param]: value
   });
 }
 
 function restoreOptions() {
   function setCurrentChoice(result) {
     document.querySelector("#maxHeight").value = result.maxHeight || "500";
+    // check the radio for OpenTab, set a default value if unavailable
+    openTab = result.openTab || "newTab";
+    console.log(openTab);
+    document.querySelector("#" + openTab).checked = true;
   }
 
   function onError(error) {
     console.log(`Error: ${error}`);
   }
 
-  let getting = browser.storage.sync.get("maxHeight");
+  let getting = browser.storage.sync.get(["maxHeight", "openTab"]);
   getting.then(setCurrentChoice, onError);
 }
 
 // load saved values in the options screen
 document.addEventListener("DOMContentLoaded", restoreOptions);
-// height value in options screen is autosaved whenever it's changed
-document.querySelector("#maxHeight").addEventListener("input", saveOptions);
+// options screen autosaves changes whenever inputs are touched
+document.querySelectorAll("input").forEach(item => {
+  item.addEventListener("input", saveOption);
+});
